b758a1ecbf3c3d0dd78a950d91f0cc2ac33ea908
unknown <al.s.kolesnik@gmail.com>
2024-01-27 01:34:48 +0000

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+/*plugins {
+    id("java")
+}*/
+plugins {
+    id("war")
+}
+
+group = "org.example"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    compileOnly(dependencyNotation = "jakarta.servlet:jakarta.servlet-api:6.0.0")
+    implementation("org.apache.tomcat:tomcat-catalina:10.1.18")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/module10/servlets/MyServletTime.java
@@ -0,0 +1,27 @@
+package module10.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+@WebServlet(value = "/time")
+public class MyServletTime extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req,
+                         HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        String timezoneParam = req.getParameter("timezone");
+        ZoneId zoneId = (timezoneParam != null && !timezoneParam.isEmpty()) ? ZoneId.of(timezoneParam) : ZoneId.of("UTC");
+        String currentTime = ZonedDateTime.now(zoneId).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z"));
+        resp.getWriter().write(currentTime);
+        resp.getWriter().close();
+    }
+
+}
